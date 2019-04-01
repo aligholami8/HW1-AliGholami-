3f78d283db635885ac84e1484e7affe069ec343c
Ali <aghgholami0@gmail.com>
2019-04-01 20:26:29 +0330

    HW1-2

---
+++ date/date/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current date & time is {0}", DateTime.Now);
+            Console.WriteLine("The current date is {0:dd / MM / yyy}", DateTime.Now);
+            
+            Console.Read();
+        }
+    }
+}
